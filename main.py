1ebcf13f460be3302f3fe7d3ab03b24b1e716457
Pooja Parupalli <poojanikita3@gmail.com>
2017-04-08 14:09:59 -0500

    validated sign up page

--- main.py
+++ main.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 import webapp2
+import re
 
 userform = """
 <form method="post">
@@ -26,8 +27,9 @@ userform = """
         </label>
       </td>
       <td>
-          <input type = "text" name="username" value="username">
+          <input type = "text" name="username" value = %(username)s>
       </td>
+      <td> %(errorUsername)s</td>
     </tr>
 
     <tr>
@@ -36,8 +38,9 @@ userform = """
         </label>
       </td>
       <td>
-        <input type = "password" name="password">
+        <input type = "password" name="password" value = %(password)s>
       </td>
+      <td> %(errorPassword)s</td>
     </tr>
 
     <tr>
@@ -46,8 +49,9 @@ userform = """
         </label>
       </td>
       <td>
-        <input type = "password" name="verifypw">
+        <input type = "password" name="verify" value = %(verify)s>
       </td>
+      <td> %(errorVerify)s</td>
     </tr>
 
     <tr>
@@ -56,8 +60,9 @@ userform = """
         </label>
       </td>
       <td>
-        <input type = "text" name="email">
+        <input type = "text" name="email" value =%(email)s>
       </td>
+      <td> %(errorEmail)s</td>
     </tr>
 </table>
 
@@ -65,9 +70,67 @@ userform = """
 </form>
 """
 
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PASSWORD_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
 class MainHandler(webapp2.RequestHandler):
-    def user_form(self, username ="", password="",verifypw="",email=""):
-        self.response.write(form)
+#main user-sign up page and renders back here if errors
+    def writeForm(self,username="",password="",verify="",email="",errorUsername="",errorPassword="",errorVerify="",errorEmail=""):
+        self.response.write(userform%{
+                                        "username":(username),
+                                        "password":(password),
+                                        "verify":(verify),
+                                        "email":(email),
+                                        "errorUsername":(errorUsername),
+                                        "errorPassword":(errorPassword),
+                                        "errorVerify":(errorVerify),
+                                        "errorEmail":(errorEmail)})
+
+    def get(self):
+        self.writeForm()
+
+    def post(self):
+        errorUsername =""
+        errorPassword = ""
+        errorVerify =  ""
+        errorEmail = ""
+
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        usernameChk = valid_username(username)
+        passwordChk = valid_password(password)
+        emailChk = valid_email(email)
+
+        if not (usernameChk and passwordChk and emailChk and password == verify):
+            if not usernameChk:
+                errorUsername = "Try again, invalid username"
+            if not passwordChk:
+                errorPassword = "You need a better password that\'s too easy to hack"
+            if not emailChk:
+                errorEmail = "That\'s not a real email, you can\'t trick me!"
+            if not password == verify:
+                errorVerify = "Your passwords don\'t match...come on, you just typed it 2 seconds ago..."
+            self.writeForm(username,password,verify,email,errorUsername,errorPassword,errorVerify,errorEmail)
+
+        else:
+            self.response.write("Welcome" + username)
+
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
